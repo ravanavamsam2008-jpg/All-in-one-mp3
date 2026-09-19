74af547125c97d2d3d46d5107749d34e5223d258
Sadauesan M <ravanavamsam2008@gmail.com>
2026-09-19 10:18:24 +0530

    app.py

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def download_audio():
         return render_template('index.html', message=f"Error: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
